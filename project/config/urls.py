79a3adb124a4a7e52c7e489cc05555b9cbaf4c75
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2023-06-29 20:23:08 -0300

    templates

--- project/config/urls.py
+++ project/config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+
+# ! include sirve para traer urls de las aplicaciones
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("cliente.urls")),
+    path("producto/", include("producto.urls")),
 ]
